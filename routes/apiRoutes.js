7fa1db4330b1d8a6ae74a071bb40f8ce37feb812
joshua glazer <jglazer@usc.edu>
2015-08-16 00:13:47 -0700

    another test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+function verifyToken( req, res, next )
+{
+	token = req.body.token;
+	if( token != process.env.INCOMING_KEY )
+	{
+		res.status( 403 ).send( "Error: wrong incoming key" )
+	}
+	else
+	{
+		next();
+	}
+}
+
 /* GET users listing. */
-router.get('/ntest', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/ntest', function(req, res, next) 
+{
+	var body = req.body;
+
+	res.send( JSON.stringify( body ) );
+	//should be middleware?
+	
+
 });
 
 module.exports = router;
+
+/*
+token=gIkuvaNzQIHg97ATvDxqgjtO
+team_id=T0001
+team_domain=example
+channel_id=C2147483705
+channel_name=test
+user_id=U2147483697
+user_name=Steve
+command=/weather
+text=94070
+*/
