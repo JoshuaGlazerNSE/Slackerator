971b54a0fc02c86f7f38e72b5bae5f86317a261c
joshua glazer <jglazer@usc.edu>
2015-08-16 01:19:30 -0700

    maybe send jira?

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,66 @@
+var request = require( 'request' );
+
 var express = require('express');
 var router = express.Router();
 
-function verifyToken( req, res, next )
+function verifyToken( token )
 {
-	token = req.body.token;
-	if( token !== process.env.INCOMING_KEY )
+	return function( req, res, next )
 	{
-		console.log( "token is " + token + " and expected is " + process.env.INCOMING_KEY );
-		res.status( 403 ).send( "Error: wrong incoming key" )
-	}
-	else
-	{
-		next();
+		if( req.body.token !== token )
+		{
+			console.log( "token is " + req.body.token + " and expected is " + token );
+			res.status( 401 ).send( "Error: wrong incoming key" )
+		}
+		else
+		{
+			next();
+		}
 	}
 }
 
 /* GET users listing. */
-router.post('/necho', verifyToken, function(req, res, next) 
+router.post('/necho', verifyToken( process.env.NECHO_TOKEN ), function(req, res) 
 {
 	var body = req.body;
-
 	delete body.token;
 	res.send( JSON.stringify( body ) );
 });
+
+router.post( '/jira', verifyToken( process.env.JIRA_TOKEN ), function( req, res )
+{
+	var body = req.body;
+
+	var summary = body.text;
+
+	var issue =
+	{
+		"fields": 
+		{
+			"project":
+			{ 
+				"key": "SCRAP"
+			},
+			"summary": summary,
+			"issuetype": 
+			{
+				"name": "Bug"
+			}
+		}
+   }
+	
+	request(
+	{
+		method: 'POST',
+		uri: "https://" + process.env.JIRA_BASIC_AUTH + "nakedsky.atlassian.net/rest/api/2/issue/",
+		json: true,
+		body: issue
+	}, function( error, data, body )
+	{
+		res.send( body );
+	} );
+
+} );
 
 module.exports = router;
 
@@ -36,4 +74,9 @@ user_id=U2147483697
 user_name=Steve
 command=/weather
 text=94070
+
+
+
+
 */
+
